c3a8de8ae05c73fd3cffea31958e5d112ae1071e
Jonas Bindrim <jonas.bindrim@hotmail.de>
2024-05-27 18:40:49 +0200

    Implement validate command

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,24 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[clap(author, version, about)]
 pub struct CliParameters {
-    /// Path to the project file
-    pub projectfile: String,
-    /// Name of the job to execute
-    pub job: String,
+    #[clap(subcommand)]
+    pub mode: Mode,
+}
+
+#[derive(Subcommand)]
+pub enum Mode {
+    /// Check whether a given project file is valid in regards to the schema
+    Validate {
+        /// The path to the project file
+        projectfile: String, 
+    },
+    /// Execute a job from a project file
+    Run {
+        /// Path to the project file
+        projectfile: String,
+        /// Name of the job to execute
+        job: String,
+    }
 }
