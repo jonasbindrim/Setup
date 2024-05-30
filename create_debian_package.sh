384f892c787239b499c9dbf2c74af590de7d8cb7
Jonas Bindrim <jonas.bindrim@hotmail.de>
2024-05-30 08:17:01 +0200

    Statically link executable

--- create_debian_package.sh
+++ create_debian_package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compile `Setup` program
-cargo build --release
+RUSTFLAGS="-C target-feature=+crt-static" cargo build --release --target x86_64-unknown-linux-gnu
 
 # Copy exectuable to `setup-deb/bin/` directory
-cp target/release/setup setup-deb/bin/
+cp target/x86_64-unknown-linux-gnu/release/setup setup-deb/bin/
 
 # Build deb package
 dpkg-deb --root-owner-group --build setup-deb
