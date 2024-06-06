d535a8f3dafd72e6e9db66b7bc342e24dbd54234
Jonas Bindrim <jonas.bindrim@hotmail.de>
2024-06-06 09:21:58 +0200

    Include jsonschema in deb-package

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ A debian package is currently only available for the amd64-architecture.
 To install the debian package, download the .deb-file and run:
 
 ```bash
-sudo dpkg -i setup.deb
+sudo dpkg -i <setupfilename>
 ```
 
 ## Installing with cargo
@@ -19,4 +19,25 @@ To install setup with cargo, clone this repository and run the following command
 
 ```bash
 cargo install --path .
+```
+
+## Schemafile
+
+After installation the project schemafile is available in `/usr/share/setup/projectfileschema.json`.
+This file can be used with editors supporting json schema validation for a better experience when creating
+the project file.
+
+Example in VS-Code:
+
+Add the following to your `settings.json` file for schema validation.
+
+```json
+{
+    "json.schemas": [
+        {
+            "fileMatch": ["<filetomatch>"],
+            "url": "/use/share/setup/projectfileschema.json"
+        }
+    ]
+}
 ```
--- create_debian_package.sh
+++ create_debian_package.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+# Test if the number of arguments is correct
+if test "$#" -ne 1; then
+    echo "Version number is missing."
+    echo "Use the version number as an argument."
+    exit 1
+fi
+
+PACKAGENAME=setup_$1_amd64.deb
+
 # Compile `Setup` program
 RUSTFLAGS="-C target-feature=+crt-static" cargo build --release --target x86_64-unknown-linux-gnu
 
-# Copy exectuable to `setup-deb/bin/` directory
-cp target/x86_64-unknown-linux-gnu/release/setup setup-deb/bin/
+# Copy exectuable to `deb-packager/bin/` directory
+mkdir deb-packager/bin
+cp target/x86_64-unknown-linux-gnu/release/setup deb-packager/bin/
+
+# Copy Jsonschema file to `deb-packager/usr/share/setup/` directory
+mkdir -p deb-packager/usr/share/setup/
+cp jsonschema.json deb-packager/usr/share/setup/projectfileschema.json
 
 # Build deb package
-dpkg-deb --root-owner-group --build setup-deb
+dpkg-deb --root-owner-group --build deb-packager $PACKAGENAME
 
 # Move deb package to `target/` directory
-mv setup-deb.deb target/setup.deb
+mv $PACKAGENAME target/
+
+# Remove temp directories
+rm -rf deb-packager/bin
+rm -rf deb-packager/usr
